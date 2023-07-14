0dded14a3951d4e429804aca89be538e15c43b98
kamoasko <kamdesigning@gmail.com>
2023-07-14 23:06:47 +0400

    New h2 added to contact page

--- src/Pages/ContactPage/index.jsx
+++ src/Pages/ContactPage/index.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import Contact from "../../Components/Contact";
+import "./contact-page.css";
+import Breadcrumb from "../../Components/Breadcrumb";
+import contact from "../../assets/images/Contact/contact.png";
+import ContacForm from "../../Components/ContactForm";
 
 const ContactPage = () => {
-  return <Contact />;
+  return (
+    <>
+      <Breadcrumb />
+      <section className="contact-page">
+        <div className="container">
+          <h2>CONTACT</h2>
+          <div className="contact__content flex">
+            <div className="contact__form">
+              <ContacForm />
+            </div>
+            <div className="contact__image">
+              <img src={contact} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ContactPage;
