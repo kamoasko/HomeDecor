187a67414a25c7b2a95803ff9a0e8db7c9f7cda1
kamoasko <kamdesigning@gmail.com>
2023-07-16 16:50:48 +0400

    authentication page created

---
+++ src/Components/AuthLayout/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AutLayout = ({ children }) => {
+  return <div>AutLayout</div>;
+};
+
+export default AutLayout;
---
+++ src/Pages/Authentication/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./authentication.css";
+
+const Authentication = () => {
+  return <div>Authentication</div>;
+};
+
+export default Authentication;
