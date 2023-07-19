44030da93ecc3adb2263ac570d61a0c002c712c6
kamoasko <kamdesigning@gmail.com>
2023-07-19 18:39:47 +0400

    breadcrumb edited

--- src/Components/Breadcrumb/index.jsx
+++ src/Components/Breadcrumb/index.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./breadcrumb.css";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useMatches } from "react-router-dom";
 
 const Breadcrumb = () => {
   const url =
     useLocation().pathname.slice(1)[0].toUpperCase() +
     useLocation().pathname.slice(2);
+  const locations = useLocation()
+    .pathname.split("/")
+    .slice(1)
+    .map((location) => location);
+  console.log(locations);
 
   return (
     <div className="container">
       <ol className="breadcrumb flex">
-        <li>
-          <Link to="/">Home</Link>
+        <li className="breadcrumb__link">
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>&Iota;</li>
-        <li className="active">
-          <Link to={useLocation().pathname}>{url}</Link>
+        <li className="breadcrumb__link">
+          <NavLink to={locations[0]}>{locations[0]}</NavLink>
+        </li>
+        <li>&Iota;</li>
+        <li className="breadcrumb__link">
+          <NavLink to={useLocation().pathname}>{locations[1]}</NavLink>
         </li>
       </ol>
     </div>
