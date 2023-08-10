77fa666e8cee9028c81ed25a44c9dd83a4be9a94
kamoasko <kamdesigning@gmail.com>
2023-08-10 16:36:45 +0400

    cart icon added to secondary button

--- src/Components/SecondaryButtons/index.jsx
+++ src/Components/SecondaryButtons/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./secondary-buttons.css";
 import { Link } from "react-router-dom";
-import { BsHandbag } from "react-icons/bs";
+import { BsCart2 } from "react-icons/bs";
 
 const SecondaryButtons = ({ text, icon, link }) => {
   return (
     <Link className="btn1 flex" to={link}>
       {icon ? (
         <>
-          <BsHandbag />
+          <BsCart2 />
         </>
       ) : (
         ""
